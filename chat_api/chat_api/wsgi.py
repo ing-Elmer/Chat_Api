aad9cc129e30355711431bb5d3a1ef1fd744f767
ing-Elmer <124623695+ing-Elmer@users.noreply.github.com>
2024-02-08 18:17:22 -0600

    cambios

--- chat_api/chat_api/wsgi.py
+++ chat_api/chat_api/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from chat_api.asgi import application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_api.settings')
-
+asg_app = application
 application = get_wsgi_application()
