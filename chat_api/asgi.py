4f4e7e50088791cdc11dbc961337a30ca2cf5884
Elmer Mejias <124623695+ing-Elmer@users.noreply.github.com>
2025-03-08 13:33:10 -0600

    Update asgi.py

--- chat_api/asgi.py
+++ chat_api/asgi.py
@@ -2,15 +2,16 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
-from rooms.routing import rooms_websocket_urlpatterns
-from comments.routing import comments_websocket_urlpatterns
-from notification.routing import notifications_websocket_urlpatterns
-
+from django.conf import settings  # Asegúrate de usar la configuración correcta.
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_api.settings')
 
 application = get_asgi_application()
+
+# Realizar importaciones retrasadas
+from rooms.routing import rooms_websocket_urlpatterns
+from comments.routing import comments_websocket_urlpatterns
+from notification.routing import notifications_websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
