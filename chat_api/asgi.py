976d366e9318a9ea0ea75d8779c088f722b04400
Elmer Mejias <124623695+ing-Elmer@users.noreply.github.com>
2025-03-08 13:21:07 -0600

    Update asgi.py

--- chat_api/asgi.py
+++ chat_api/asgi.py
@@ -3,19 +3,22 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+# Establecer la configuración de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_api.settings')
 
+# Obtener la aplicación ASGI de Django (esto maneja las solicitudes HTTP)
 application = get_asgi_application()
 
-# Delay the import of routing files to avoid early loading of models
+# Retrasar la importación de las rutas de WebSocket para evitar la carga temprana de modelos
 from rooms.routing import rooms_websocket_urlpatterns
 from comments.routing import comments_websocket_urlpatterns
 from notification.routing import notifications_websocket_urlpatterns
 
+# Configurar el enrutamiento para HTTP y WebSocket
 application = ProtocolTypeRouter(
     {
-        "http": application,
-        "websocket": AuthMiddlewareStack(
+        "http": application,  # Las solicitudes HTTP se manejan con la aplicación Django normal
+        "websocket": AuthMiddlewareStack(  # Las solicitudes WebSocket requieren autenticación
             URLRouter(
                 rooms_websocket_urlpatterns + comments_websocket_urlpatterns + notifications_websocket_urlpatterns
             ),
